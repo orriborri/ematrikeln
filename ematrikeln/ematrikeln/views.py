698fab5247b0453a346ba259be321d14a005dd76
Oscar Henriksson <oscar.henriksson91@gmail.com>
2015-07-21 13:08:46 +0300

    new member form kinda ready

--- ematrikeln/ematrikeln/views.py
+++ ematrikeln/ematrikeln/views.py
@@ -6,7 +6,10 @@ def index(req):
     return(render(req,'index.html'))
 
 def add(req):
-    return(render(req,'new.html'))
+    hometownList = sorted(['Lovisa','Borgå','Sibbo','Kotka'])
+    schoolList = sorted(['Aalto Universitet','Helsingfors Universitet','Arcada','Sibelius Akademin'])
+    gymnasiumList = sorted(['Kotka svenska samskola','Lovisa Gymnasium','Borgå Gymnasium','Sibbo Gymnasium'])
+    return(render(req,'new.html',{'gymnasium':gymnasiumList,'schoolList':schoolList,'hometownList':hometownList}))
 
 def add_member(req):
     return
