bb2ce8b5ee6a00d5b8eff2567c2dc7972d7fac08
Oscar Henriksson <oscar.henriksson91@gmail.com>
2015-12-22 13:12:27 +0200

    Revert "Test"

--- ematrikeln/ematrikeln/views.py
+++ ematrikeln/ematrikeln/views.py
@@ -15,55 +15,46 @@ def add(req,state):
         success = 'false'
     else:
         success = 'true'
-    form = newMember()
-    return(render(req,'new.html',{'showdialog':success,'form':form}))
+    schoolList = School.objects.all().extra(order_by=['name'])
+    hometownList = Town.objects.all().extra(order_by=['name'])
+    gymnasiumList = Gymnasium.objects.all().extra(order_by=['name'])
+    #schoolList = sorted(['Aalto Universitet','Helsingfors Universitet','Arcada','Sibelius Akademin'])
+    #gymnasiumList = sorted(['Kotka svenska samskola','Lovisa Gymnasium','Borgå Gymnasium','Sibbo Gymnasium'])
+    return(render(req,'new.html',{'showdialog':success,'gymnasium':gymnasiumList,'schoolList':schoolList,'hometownList':hometownList}))
 
-def view_member(req,req_id):
-    member =  User.objects.get(id=req_id)
-    return(render(req,'medlem.html',{'user':member}))
-def delete_member(req,req_id):
-    member =  User.objects.get(id= req_id)
-    member.delete()
-    return redirect('/')
 def add_member(req):
-    if req.method == 'POST':
-        if req.is_valid():
-            firstName   = req.POST['firstName']
-            lastName    = req.POST['lastName']
-            address     = req.POST['address']
-            phone       = req.POST['phonenumber']
-            city        = req.POST['city']
-            postcode    = req.POST['postcode']
-            epost       = req.POST['email']
-            if req.POST['gymnasium'] != 'Annat':
-                gymnasium = req.POST['gymnasium']
-            else:
-                gymnasium = req.POST['gymnasiumAnnat']
-            if req.POST['hometown'] != 'Annat':
-                hometown = req.POST['hometown']
-            else:
-                hometown = req.POST['hometownAnnat']
-            if req.POST['school'] != 'Annat':
-                school = req.POST['school']
-            else:
-                school = req.POST['schoolAnnat']
-            study = req.POST['study']
-            endYear = req.POST['endYear']
-            startYear = req.POST['startYear']
-            active = req.POST['active']
-            # medlemsavgift = req.POST['Betalat']
-            studyLine = StudyLine.objects.get_or_create(school = School.objects.get_or_create(name=school)[0], name = study)
-            study = Study.objects.create(startYear = startYear,graduated = False,active=active,endYear=endYear, studyLine = studyLine[0])
-            newMember = User.objects.create(homeTown = Town.objects.get_or_create(name=hometown)[0], gymnasium = Gymnasium.objects.get_or_create(name=gymnasium)[0], study=study)
-            newMember.firstName = firstName
-            newMember.lastName = lastName
-            newMember.phone = phone
-            newMember.adress = address
-            newMember.city = city
-            newMember.email = epost
-            newMember.study = study
-            newMember.postcode = postcode
-            newMember.save()
-            return HttpResponse(200)
-        return HttpResponse(500)
-    return HttpResponse(404)
+    firstName   = req.POST['firstName']
+    lastName    = req.POST['lastName']
+    address     = req.POST['address']
+    city        = req.POST['city']
+    postcode    = req.POST['postcode']
+    epost       = req.POST['email']
+    print(type(req.POST['gymnasium']))
+
+    if req.POST['gymnasium'] != 'Annat':
+        gymnasium = req.POST['gymnasium']
+    else:
+        gymnasium = req.POST['gymnasiumAnnat']
+    if req.POST['hometown'] != 'Annat':
+        hometown = req.POST['hometown']
+    else:
+        hometown = req.POST['hometownAnnat']
+    if req.POST['school'] != 'Annat':
+        school = req.POST['school']
+    else:
+        school = req.POST['schoolAnnat']
+    study = req.POST['study']
+    print("home"+hometown)
+   # medlemsavgift = req.POST['Betalat'
+    
+    newMember = User.objects.create(school = School.objects.get_or_create(name=school)[0], homeTown = Town.objects.get_or_create(name=hometown)[0], gymnasium = Gymnasium.objects.get_or_create(name=gymnasium)[0])
+    newMember.firstName = firstName
+    newMember.lastName = lastName
+    newMember.address = address
+    newMember.city = city
+    newMember.email = epost
+    newMember.study = study
+    newMember.poscode = postcode
+
+    newMember.save()
+    return redirect('/add/success')
